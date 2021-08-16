1cc19a20cc12960b88da5779b35d333659c42f3c
suzuryo <suzuryo@gmail.com>
2021-08-16 19:31:30 +0900

    iwate-ninshou.jpのURLを出力する

--- bin/iwateNinshouRestaurant2csv.rb
+++ bin/iwateNinshouRestaurant2csv.rb
@@ -9,11 +9,11 @@ res = Net::HTTP.get(URI('https://api.iwate-ninshou.jp/shop/'))
 data = JSON.parse(res)['data']
 
 csv = [
-  ['店名', 'lat', 'lon', 'url', '住所']
+  ['店名', 'lat', 'lon', '詳細', '住所']
 ]
 
 data.each do |d|
-  csv << [d['shop_name'], d['shop_lat'], d['shop_lon'], d['shop_url'], d['shop_address']]
+  csv << [d['shop_name'], d['shop_lat'], d['shop_lon'], "https://iwate-ninshou.jp/detail.html?id=#{d['id']}", d['shop_address']]
 end
 
 File.write('tsv/restaurant.csv', csv.map(&:to_csv).join)
