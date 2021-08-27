db6699776be8eacc0955f2d6b276f81581e3d236
suzuryo <suzuryo@gmail.com>
2021-08-28 02:18:05 +0900

    レストランを2000軒ごとに分割する

--- bin/iwateNinshouRestaurant2csv.rb
+++ bin/iwateNinshouRestaurant2csv.rb
@@ -8,12 +8,20 @@ require 'csv'
 res = Net::HTTP.get(URI('https://api.iwate-ninshou.jp/shop/'))
 data = JSON.parse(res)['data']
 
-csv = [
-  ['店名', 'lat', 'lon', '詳細', '住所']
-]
+csv = []
+headers = ['店名', 'lat', 'lon', '詳細', '住所']
 
 data.each do |d|
   csv << [d['shop_name'], d['shop_lat'], d['shop_lon'], "https://iwate-ninshou.jp/detail.html?id=#{d['id']}", d['shop_address']]
 end
 
-File.write('tsv/restaurant.csv', csv.map(&:to_csv).join)
+# 2000行ごとにファイルを分ける
+ROW_SIZE = 2000
+
+((csv.size / ROW_SIZE) + 1 ).times do |i|
+  File.write(
+    "tsv/restaurant#{i}.csv",
+    ([headers] + csv[ROW_SIZE*i...ROW_SIZE*(i+1)]).map(&:to_csv).join
+  )
+end
+
