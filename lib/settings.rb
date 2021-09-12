fd8d5000d7f21bd41fc472a1fdefe0ca54ac9a4c
suzuryo <suzuryo@gmail.com>
2021-09-12 14:16:48 +0900

    site2tsvを削除

--- lib/settings.rb
+++ lib/settings.rb
@@ -2,19 +2,6 @@
 
 TARGET_MIN_ID = 849
 
-SITES = {
-  iwate: {
-    url: 'https://www.pref.iwate.jp/kurashikankyou/iryou/covid19/1046015/index.html',
-    selector: '#voice > table > tbody > tr > td:nth-child(1) a',
-    regex: %r{^https://www.pref.iwate.jp}
-  },
-  morioka: {
-    url: 'http://www.city.morioka.iwate.jp/kenkou/kenko/1031971/1032075/1036303/index.html',
-    selector: '#voice > ul > li > a',
-    regex: %r{^http://www.city.morioka.iwate.jp}
-  }
-}.freeze
-
 PDFS = {
   iwate: {
     url: 'https://www.pref.iwate.jp/kurashikankyou/iryou/covid19/1046698.html',
@@ -30,19 +17,4 @@ PDFS = {
     pdf_dir: File.expand_path(File.join(__dir__, '../download/pdf/morioka')),
     csv_dir: File.expand_path(File.join(__dir__, '../download/csv/morioka'))
   }
-}
-
-# NotValidUrlError
-class NotValidUrlError < StandardError
-end
-
-# String
-class String
-  def iwate?
-    !!(SITES[:iwate][:regex].match self)
-  end
-
-  def morioka?
-    !!(SITES[:morioka][:regex].match self)
-  end
-end
+}.freeze
