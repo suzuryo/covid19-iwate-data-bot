cab291e7862730f924257d9e19cf5021e4d1afe9
suzuryo <suzuryo@gmail.com>
2021-09-14 14:39:42 +0900

    gcf から site2tsv を削除する

--- app.rb
+++ app.rb
@@ -10,7 +10,6 @@ require 'json'
 require "sinatra/base"
 require 'typhoeus'
 require_relative './lib/settings'
-require_relative './lib/site'
 require_relative './lib/twitter'
 
 ENDPOINT_URL = 'https://api.twitter.com/2/users/:id/tweets'
@@ -46,32 +45,6 @@ class App < Sinatra::Base
 
     "検査件数\n#{@main_summary1}\n\n検査陽性者の状況\n#{@main_summary2}\n\n陽性者\n#{@positive_cases}"
   end
-
-  get '/site2tsv' do
-    id = recent_id
-    data = SITES.map do |_city, site|
-      Site2Tsv::Site.new(site: site, id: id).data
-    end.flatten
-
-    # 文字列組み立て
-    @patients = ''
-    prev_id = id
-    data.sort_by { |a| a['id'] }.uniq.each do |b|
-      @patients += "\n" * (b['id'] - prev_id)
-      @patients += "#{b['id']}\t#{b['リリース日']}\t#{b['確定日']}\t#{b['発症日']}\t#{b['無症状']}\t#{b['年代']}\t#{b['性別']}\t#{b['居住地']}\t#{b['滞在地']}\t#{b['入院日']}\t#{b['url']}\t#{b['接触歴']}"
-      prev_id = b['id']
-    end
-
-    "陽性者の状況\n\n#{@patients}"
-  end
-end
-
-def recent_id
-  json = JSON.parse(URI.open('https://raw.githubusercontent.com/MeditationDuck/covid19/development/data/data.json').read)
-  json['patients']['data']
-    .filter { |a| Time.parse(a['確定日']) < Time.parse(json['patients']['data'].last['確定日']) }
-    .max_by { |a| a['id'] }['id']
-    .to_i + 1
 end
 
 FunctionsFramework.http "covid19-iwate-data-tsv" do |request|
