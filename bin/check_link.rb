5e574d01ef190941fde6b76c059881c941abd774
suzuryo <suzuryo@gmail.com>
2022-08-30 14:32:47 +0900

    change pdf file

--- bin/check_link.rb
+++ bin/check_link.rb
@@ -6,7 +6,7 @@ require 'typhoeus'
 require 'slack-notifier'
 
 urls = %w[
-  https://www.pref.iwate.jp/_res/projects/default_project/_page_/001/052/938/shinryokensalist_040820-3.pdf
+  https://www.pref.iwate.jp/_res/projects/default_project/_page_/001/052/938/040822_itiran3.pdf
 ]
 
 def check_urls(urls)
@@ -32,7 +32,7 @@ def check_urls(urls)
   requests.map do |request|
     hexdigest = Digest::SHA256.hexdigest(request.response.body)
     response_code = request.response.response_code
-    slack_msg += "#{Time.now}\n#{hexdigest}\n" if hexdigest != '91f9f5fc5ce23e19c5433abf6e260b749cc891fce93b57fa90abcd7d599b91f7'
+    slack_msg += "#{Time.now}\n#{hexdigest}\n" if hexdigest != 'fa129c17d2c78d2b066df0d30994c51fe8a50f1ae76f177a9785242c183da27d'
     slack_msg += "#{Time.now}\n#{response_code} #{request.base_url}\n" if response_code != 200
   end
 
