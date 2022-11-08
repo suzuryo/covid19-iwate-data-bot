c38af3baf4a816102e47206341f3ee9a78767dd1
suzuryo <suzuryo@gmail.com>
2022-11-08 14:04:21 +0900

    change pdf hexdigest

--- bin/check_link.rb
+++ bin/check_link.rb
@@ -8,7 +8,7 @@ require 'slack-notifier'
 urls = [
   {
     url: 'https://www.pref.iwate.jp/_res/projects/default_project/_page_/001/052/938/shinryokensalist_041102-4.pdf',
-    hexdigest: '05fd2559ee959d90f81dd326eae36a0427c82c1c86ceb9c83cb5236615dc4b44'
+    hexdigest: 'ab2cf075c35934a0910ad8471eedf3d6a04128c8b57e920aeb34c8fa47b3c6ed'
   }
 ]
 
